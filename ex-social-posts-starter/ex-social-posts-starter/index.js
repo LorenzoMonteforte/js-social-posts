0bcde100185d69c9addf09db783601f752735011
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-11-14 15:54:16 +0100

    Milestone 3

--- ex-social-posts-starter/ex-social-posts-starter/index.js
+++ ex-social-posts-starter/ex-social-posts-starter/index.js
@@ -3,7 +3,7 @@ const post = [
     {
         id : 1,
         nome : "Lorenzo Monteforte",
-        foto : "img/user1.avif",
+        foto : "img/user1.jpg",
         data : "2023-11-14",
         testo: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
         immagine : "img/immagine1.webp",
@@ -12,7 +12,7 @@ const post = [
     {
         id : 2,
         nome : "Alessio Tosoni",
-        foto : "img/user2.avif",
+        foto : "img/user2.webp",
         data : "2023-11-13",
         testo : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
         immagine : "",
@@ -21,7 +21,7 @@ const post = [
     {
         id : 3,
         nome : "Andrea Bartoloni",
-        foto : "img/user3.avif",
+        foto : "img/user3.png",
         data : "2023-11-12",
         testo : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
         immagine : "img/immagine3.avif",
@@ -30,34 +30,36 @@ const post = [
 ];
 /* Post di esempio da generare con JS */
 const container = document.getElementById("container");
-const codiceHTML = `<div class="post">
-                        <div class="post__header">
-                            <div class="post-meta">                    
-                                <div class="post-meta__icon">
-                                    <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+for(let i=0; i<post.length; i++){
+    const codiceHTML = `<div class="post">
+                            <div class="post__header">
+                                <div class="post-meta">                    
+                                    <div class="post-meta__icon">
+                                        <img class="profile-pic" src="${post[i].foto}" alt="">                    
+                                    </div>
+                                    <div class="post-meta__data">
+                                        <div class="post-meta__author">${post[i].nome}</div>
+                                        <div class="post-meta__time">${post[i].data}</div>
+                                    </div>                    
                                 </div>
-                                <div class="post-meta__data">
-                                    <div class="post-meta__author">Phil Mangione</div>
-                                    <div class="post-meta__time">4 mesi fa</div>
-                                </div>                    
                             </div>
-                        </div>
-                        <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-                        <div class="post__image">
-                            <img src="https://unsplash.it/600/300?image=171" alt="">
-                        </div>
-                        <div class="post__footer">
-                            <div class="likes js-likes">
-                                <div class="likes__cta">
-                                    <a class="like-button  js-like-button" href="#" data-postid="1">
-                                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                                        <span class="like-button__label">Mi Piace</span>
-                                    </a>
-                                </div>
-                                <div class="likes__counter">
-                                    Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
-                                </div>
-                            </div> 
-                        </div>            
-                    </div>`;
-container.innerHTML = codiceHTML;
+                            <div class="post__text">${post[i].testo}</div>
+                            <div class="post__image">
+                                <img src="${post[i].immagine}" alt="">
+                            </div>
+                            <div class="post__footer">
+                                <div class="likes js-likes">
+                                    <div class="likes__cta">
+                                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                            <span class="like-button__label">Mi Piace</span>
+                                        </a>
+                                    </div>
+                                    <div class="likes__counter">
+                                        Piace a <b id="like-counter-1" class="js-likes-counter">${post[i].like}</b> persone
+                                    </div>
+                                </div> 
+                            </div>            
+                        </div>`;
+    container.innerHTML +=  codiceHTML;
+}
