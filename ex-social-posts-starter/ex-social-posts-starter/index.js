0a886a24aaf48ff904c0b9a2361fdf2f3b820f2b
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-11-14 15:11:52 +0100

    Milestone 1

---
+++ ex-social-posts-starter/ex-social-posts-starter/index.js
@@ -0,0 +1,30 @@
+/* Array post */
+const post = [
+    {
+        id : 1,
+        nome : "Lorenzo Monteforte",
+        foto : "img/user1.avif",
+        data : "2023-11-14",
+        testo: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        immagine : "img/immagine1.webp",
+        like : 10
+    },
+    {
+        id : 2,
+        nome : "Alessio Tosoni",
+        foto : "img/user2.avif",
+        data : "2023-11-13",
+        testo : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        immagine : "",
+        like : 50
+    },
+    {
+        id : 3,
+        nome : "Andrea Bartoloni",
+        foto : "img/user3.avif",
+        data : "2023-11-12",
+        testo : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        immagine : "img/immagine3.avif",
+        like : 100
+    }
+];
